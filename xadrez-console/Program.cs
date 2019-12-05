7d40169f8aac57f020e15648dba633f09ce38ff1
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2019-12-04 21:51:24 -0300

    Criado a classe posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿ using System;
+using Tabuleiro;
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Posicao P;
+            P = new Posicao(3, 4);
         }
     }
 }
